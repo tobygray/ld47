d13fbd5bc7f9fc0e63721f4c1cee640a14faa86e
fflexo <fflexo@users.noreply.github.com>
2020-09-26 23:42:44 +0000

    Create app

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+
+console.log("HI")
+
 import * as PIXI from 'pixi.js'
 
 let type = "WebGL"
@@ -6,3 +9,22 @@ if(!PIXI.utils.isWebGLSupported()){
 }
 
 PIXI.utils.sayHello(type)
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  let app = new PIXI.Application({
+    width: 256,         // default: 800
+    height: 256,        // default: 600
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1       // default: 1
+  });
+
+  document.body.appendChild(app.view);
+
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+  app.renderer.autoResize = true;
+  app.renderer.resize(window.innerWidth/1.25, window.innerHeight/1.25);
+
+});

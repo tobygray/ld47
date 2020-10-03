c33179f7a5e91d598eae6c30b6fcb86e6731f7aa
fflexo <fflexo@users.noreply.github.com>
2020-10-03 18:39:20 +0100

    JSON text now sorta works

---
+++ src/editor.js
@@ -0,0 +1,48 @@
+const $ = require('jquery');
+
+function getTextElement() {
+  return $('form#editor textarea#json');
+}
+
+function getCurrentJson() {
+  return getTextElement().val();
+}
+
+function getCurrentArray() {
+  const json = getCurrentJson();
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    console.log('Returning default, parse failed', json);
+    return [];
+  }
+}
+
+function setCurrentJSON(str) {
+  getTextElement().val(str);
+}
+
+function setCurrentArray(arr) {
+  setCurrentJSON(JSON.stringify(arr));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('editor running');
+  $('form#editor input').on('click', (evt) => {
+    // Input buttons just change the text area and hook off that
+    console.log('Click on: ', evt.target.value);
+    let arr = getCurrentArray();
+    console.log('Current array is: ', arr);
+    if (evt.target.value !== '<--') {
+      arr = arr.concat(evt.target.value);
+    } else {
+      arr.pop();
+    }
+    setCurrentArray(arr);
+    console.log('New array is: ', arr);
+  });
+
+  $('form#editor textarea#json').on('change keyup paste', (evt) => {
+    console.log('Text area changed: ', evt.target.value);
+  });
+});

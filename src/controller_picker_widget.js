fdd963d3ea6f8207d38a85972e4ffa95e4b952ca
Toby <toby.gray@gmail.com>
2020-10-03 15:15:26 +0000

    Add instructions to setup screen

--- src/controller_picker_widget.js
+++ src/controller_picker_widget.js
@@ -4,6 +4,15 @@ import ControllerSelection from './controller_selection';
 
 const MAX_PLAYERS = 4;
 
+const style = new PIXI.TextStyle({
+  fontFamily: 'Arial',
+  fontSize: 32,
+  fill: ['#ffffff', '#00ff99'],
+  stroke: '#4a1850',
+  strokeThickness: 3,
+  lineJoin: 'round',
+});
+
 class ControllerPicker extends PIXI.Container {
   constructor(app, raceState) {
     super();
@@ -13,6 +22,11 @@ class ControllerPicker extends PIXI.Container {
     this.newControllerListener = new NewControllerListener((controller) => {
       this.handleNewController(controller);
     });
+
+    const instructionText = new PIXI.Text('Press space or a button on a controller to add a player', style);
+    instructionText.anchor.set(0.5, 0.5);
+    instructionText.position.set(this.app.renderer.width / 2, 600);
+    this.addChild(instructionText);
   }
 
   handleNewController(controller) {
--- src/controller_selection.js
+++ src/controller_selection.js
@@ -1,5 +1,21 @@
 import * as PIXI from 'pixi.js';
 
+const style = new PIXI.TextStyle({
+  fontFamily: 'Arial',
+  fontSize: 48,
+  fontStyle: 'italic',
+  fontWeight: 'bold',
+  fill: ['#ffffff', '#00ff99'], // gradient
+  stroke: '#4a1850',
+  strokeThickness: 5,
+  dropShadow: true,
+  dropShadowColor: '#000000',
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+  lineJoin: 'round',
+});
+
 class ControllerSelection {
   constructor(app, container, controller, idx, maxPlayers) {
     this.app = app;
@@ -11,13 +27,17 @@ class ControllerSelection {
       PIXI.utils.TextureCache[controller.icon],
     );
 
+    const xPos = (this.app.renderer.width * (idx + 1)) / (maxPlayers + 1);
+    const yPos = 700;
     this.controllerSprite.anchor.set(0.5, 0.5);
-    this.controllerSprite.position.set(
-      (this.app.renderer.width * (idx + 1)) / (maxPlayers + 1),
-      700,
-    );
-
+    this.controllerSprite.position.set(xPos, yPos);
     this.container.addChild(this.controllerSprite);
+
+    const playerText = new PIXI.Text(`Player ${idx + 1}`, style);
+    playerText.anchor.set(0.5, 0.5);
+    playerText.x = xPos;
+    playerText.y = yPos + this.controllerSprite.height + 10;
+    this.container.addChild(playerText);
 
     this.controller.setChangeListener(() => {
       this.controllerSprite.rotation = (Math.PI * this.controller.value) / 2;

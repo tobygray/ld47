160f2901127f374b8269ac94f4d9c84790d040fa
fflexo <fflexo@users.noreply.github.com>
2020-10-04 12:37:37 +0100

    All races are now 3 lap races

--- src/race_results.js
+++ src/race_results.js
@@ -1,3 +1,5 @@
+const LAP_TARGET = 3;
+
 export default class RaceResults {
   constructor(raceOverCallback) {
     this.laptimes = [[], []];
@@ -5,10 +7,28 @@ export default class RaceResults {
     this.crashcounts = [0, 0];
     this.startTime = 0;
     this.endTime = undefined;
-    this.raceOverCallback = raceOverCallback;
+    this.endRace = raceOverCallback;
   }
 
   onCarMovedPiece(car) {
-    console.log(this, car);
+    if (car.currentTrack === 0) {
+      // Just started a new lap
+      this.lapcounts[car.playerIndex] += 1;
+      console.log('Car completed a lap: ', car);
+    }
+
+    this.checkEndConditions();
+  }
+
+  // TODO: subclass this an then let other people implement other rules...
+  shouldEndRace() {
+    return !this.lapcounts.every((count) => count < LAP_TARGET);
+  }
+
+  checkEndConditions() {
+    if (this.shouldEndRace()) {
+      // TODO: pause before calling this so people can see end before insta hitting results screen?
+      this.endRace();
+    }
   }
 }

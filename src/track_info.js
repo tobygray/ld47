80cc253df7386caa06ea86f0de5c799b00a333f5
Sam <ludumdare@sentynel.com>
2020-10-04 22:33:30 +0100

    specify scale and pos of tracks

--- src/track_info.js
+++ src/track_info.js
@@ -9,16 +9,25 @@ const TRACK_INFO = [
       's4', 's4',
       'r2', 'r2', 'r2', 'r2', 'r2', 'r2', 'r2', 'r2',
     ],
+    scale: 0.8,
+    x: 700,
+    y: 250,
   },
   {
     name: 'LOOPER TWO',
     preview_file: 'assets/tracks/2/preview.png',
     pieces: ['ss', 'ss', 'ss', 'ss', 'ss', 'ss', 'ss', 'ss', 's4', 'r4', 'r3', 'r2', 'r1', 's', 's', 'l2', 'l2', 'l1', 'l1', 'l1', 'l1', 'ss', 'ss', 's4', 's', 's', 'l2', 'l2', 'l2', 'ss', 's', 's', 's', 'l1', 'l1', 'l1', 's4', 's4', 's', 'l2', 'l2', 'ss', 'r1', 'r1', 's4', 'l1', 'l1', 'l1', 'l1', 's', 'ss', 's4', 's', 's', 'r3', 'r2', 'r2', 'r1', 'r1', 'ss', 'ss', 'ss', 'r2', 'r1', 'r1', 'ss', 'ss', 'ss', 'ss', 'ss', 'ss', 'r1', 'r1', 'r1', 'r1', 's', 's', 's'],
+    scale: 0.5,
+    x: 400,
+    y: 400,
   },
   {
     name: 'LOOPTY TREE',
     preview_file: 'assets/tracks/3/preview.png',
     pieces: ['s4', 'r3', 'r3', 'r2', 'r2', 'r2', 'r2', 'r2', 'r3', 'r3', 'r4', 's', 's', 'l2', 'l2', 'l2', 'l2', 'l2', 'l2', 'l3', 'l3', 'l3', 'l3', 's4', 's4', 's4', 's4', 's', 's', 'l3', 'l3', 'l3', 'l3', 's4', 's', 'l2', 'l2', 'l2', 'l1', 'l1', 'l2', 'l2', 'l2', 'l2', 'r3', 'r2', 'r2', 'r2', 'r2', 's4', 's4', 'r4', 'ss', 's4', 'r3', 'r2', 'r2', 'r2', 'r2', 'r3', 'r4', 's', 'ss'],
+    scale: 0.5,
+    x: 700,
+    y: 250,
   },
 ];
 

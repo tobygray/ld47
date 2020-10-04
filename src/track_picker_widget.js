8f8c711ce1a81ce7ff6caf92aa83b602a00ecc47
Toby <toby.gray@gmail.com>
2020-10-04 15:58:16 +0100

    Show pre-selected track

--- src/track_picker_widget.js
+++ src/track_picker_widget.js
@@ -1,6 +1,9 @@
 import * as PIXI from 'pixi.js';
 import TRACK_INFO from './track_info';
 
+const SELECTED_TINT = 0xFFFFFF;
+const UNSELECTED_TINT = 0xAAAAAA;
+
 function createTrackPicker(app, raceConfig) {
   const container = new PIXI.Container();
 
@@ -18,19 +21,32 @@ function createTrackPicker(app, raceConfig) {
     currentTrackTexture.buttonMode = true;
     currentTrackTexture.interactive = true;
 
-    currentTrackTexture.on('pointertap', (_evt) => {
-      // Step 1: deselect all the tracks:
+    const setActive = () => {
+      // Add a border on the current clicked one.
+      currentTrackTexture.scale.set(1.5, 1.5);
+      currentTrackTexture.tint = SELECTED_TINT;
+    };
+    const buttonSelectedCallback = (_evt) => {
+      // Deselect all the tracks:
       trackSprites.forEach((anOldTrack) => {
         anOldTrack.scale.set(1, 1);
-        anOldTrack.tint = 0xAAAAAA; // Fade a bit
+        anOldTrack.tint = UNSELECTED_TINT;
       });
 
-      // Step 2: add a border on the current clicked one now we've removed them all
-      currentTrackTexture.scale.set(1.5, 1.5);
-      currentTrackTexture.tint = 0xFFFFFF; // No tint
+      // Highlight this one.
+      setActive();
+
       // Record the choice!
       raceConfig.track = track;
-    });
+    };
+    currentTrackTexture.on('pointertap', buttonSelectedCallback);
+
+    if (raceConfig.track && track === raceConfig.track) {
+      // Has a previous configuration which has this track selected.
+      setActive();
+    } else {
+      currentTrackTexture.tint = UNSELECTED_TINT;
+    }
 
     return currentTrackTexture;
   });

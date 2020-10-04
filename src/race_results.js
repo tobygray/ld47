528d8b955632fc0502d3358882c94e20ae591647
fflexo <fflexo@users.noreply.github.com>
2020-10-04 14:27:03 +0100

    laptiming actually works!

--- src/race_results.js
+++ src/race_results.js
@@ -4,6 +4,8 @@ const LAP_TARGET = 3;
 
 export default class RaceResults {
   constructor(raceOverCallback) {
+    // During the race this is absolute time stamps.
+    // After the race we go through and calculate actual lap times.
     this.laptimes = [[], []];
     this.lapcounts = [0, 0];
     this.crashcounts = [0, 0];
@@ -15,14 +17,12 @@ export default class RaceResults {
   start() {
     sound.play('assets/audio/sfx/321go.mp3', {
       complete: () => {
-        console.log('321 sound done, enabling cars');
         this.enableCars();
       },
     });
   }
 
   initCars(carArray) {
-    console.log('In init cars', carArray);
     carArray.forEach((car) => {
       car.enabled = false;
     });
@@ -31,13 +31,15 @@ export default class RaceResults {
       carArray.forEach((car) => {
         car.enabled = true;
       });
+      this.startTime = Date.now();
     };
   }
 
   onCarMovedPiece(car) {
     if (car.currentTrack === 0) {
       // Just started a new lap
-      this.lapcounts[car.playerIndex] += 1;
+      this.laptimes[car.playerIndex].push(Date.now());
+      this.lapcounts[car.playerIndex] += 1; // We could just compute this from laptimes array length
       console.log('Car completed a lap: ', car);
     }
 
@@ -49,8 +51,25 @@ export default class RaceResults {
     return !this.lapcounts.every((count) => count < LAP_TARGET);
   }
 
+  computeRealLaptimes() {
+    // At the end of a race our laptimes array is a collection of times when we crossed
+    // the line. This re-computes those to be the times of each actual lap!
+    this.laptimes = this.laptimes.map((times) => {
+      const arr = [];
+      let lastTime = this.startTime;
+      times.forEach((t) => {
+        arr.push(t - lastTime);
+        lastTime = t;
+      });
+      return arr;
+    });
+  }
+
   checkEndConditions() {
     if (this.shouldEndRace()) {
+      this.computeRealLaptimes();
+      console.log(this.laptimes);
+      // TODO: let 2nd place finish?
       // TODO: pause before calling this so people can see end before insta hitting results screen?
       this.endRace();
     }

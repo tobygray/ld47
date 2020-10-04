85ce8bc605321c1bba1747796e491c52986478bc
Toby <toby.gray@gmail.com>
2020-10-04 20:27:13 +0100

    Add basic tests for DriverResult

--- test/DriverResultTest.js
+++ test/DriverResultTest.js
@@ -4,12 +4,55 @@ const { default: DriverResult } = require('../src/api/driver_result.js');
 describe('Check crash count', () => {
   const driverResult = new DriverResult();
   it('starts zero', () => {
-    assert.equal(driverResult.crashCount, 0);
+    assert.strictEqual(driverResult.crashCount, 0);
   });
   it('adds one', () => {
     driverResult.crashed();
-    assert.equal(driverResult.crashCount, 1);
+    assert.strictEqual(driverResult.crashCount, 1);
     driverResult.crashed();
-    assert.equal(driverResult.crashCount, 2);
+    assert.strictEqual(driverResult.crashCount, 2);
+  });
+});
+
+describe('Check name behaviour', () => {
+  const driverResult = new DriverResult(47);
+  it('has a default name', () => {
+    assert.strictEqual(driverResult.name, 'Player 48');
+  });
+  it('can have name set', () => {
+    const newName = 'Bob';
+    driverResult.name = newName;
+    assert.strictEqual(driverResult.name, newName);
+  });
+});
+
+describe('Check lap recording', () => {
+  const driverResult = new DriverResult();
+  it('starts with no laps', () => {
+    assert.strictEqual(driverResult.lapCount, 0);
+    assert.deepStrictEqual(driverResult.lapTimes, []);
+  });
+});
+
+describe('can serialize and deserialize', () => {
+  const expectedIndex = 123;
+  const expectedName = 'Nigel';
+  const expectedCrashCount = 7;
+  let jsonData = '';
+  it('can serialize', () => {
+    const driverResult = new DriverResult(expectedIndex);
+    driverResult.name = expectedName;
+    for (let i = 0; i < expectedCrashCount; i += 1) {
+      driverResult.crashed();
+    }
+
+    jsonData = driverResult.toJson();
+    assert.notStrictEqual(jsonData, '');
+  });
+  it('it deserializes', () => {
+    const driverResult = DriverResult.fromJson(jsonData);
+    assert.strictEqual(driverResult.index, expectedIndex);
+    assert.strictEqual(driverResult.name, expectedName);
+    assert.strictEqual(driverResult.crashCount, expectedCrashCount);
   });
 });

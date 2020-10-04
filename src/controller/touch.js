3c3f7cfb2b730ddfb144d5e0c3efa5ead533f667
Toby <toby.gray@gmail.com>
2020-10-04 12:51:51 +0100

    Include haptics feedback on mobiles

--- src/controller/touch.js
+++ src/controller/touch.js
@@ -1,8 +1,24 @@
 import ControllerBase from './controllerbase';
 
+function generateVibratePattern(lengthMs, intensity) {
+  // Use a duty cycle over the length of the specified period.
+  return [lengthMs * intensity, lengthMs * (1 - intensity)];
+}
+
 class TouchController extends ControllerBase {
   constructor(handler) {
     super(handler, 'Touch', 'touch.png');
+    this.canVibrate = 'vibrate' in window.navigator;
+  }
+
+  setDangerValue(newValue) {
+    if (!this.canVibrate) {
+      return;
+    }
+    if (newValue === 0) {
+      return;
+    }
+    window.navigator.vibrate(generateVibratePattern(50, newValue));
   }
 }
 
